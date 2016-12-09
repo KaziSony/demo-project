2f8d53e484a603f1f7b86ed2ee4f9b13331da255
KaziSony <kazihanif3@gmail.com>
2016-12-10 00:09:14 +0600

    new file added

---
+++ CalculatorApp/CalculatorApp/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Give ur number :");
+            double aNumber = Convert.ToDouble(Console.ReadLine());
+            double bNumber = Convert.ToDouble(Console.ReadLine());
+            //Add(12, 5);
+            //Console.WriteLine("Give ur number :");
+            Console.WriteLine(Add(aNumber , bNumber));
+            
+            Console.Read();
+        }
+        public static double Add(double aNumber , double bNumber)
+        {
+            return aNumber + bNumber;
+        }
+    }
+}
